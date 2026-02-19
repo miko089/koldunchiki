deda7821252cfe7b33fa76da5d110b4caa04a19f
Mikhail Maliugin <ghastmisha089@gmail.com>
2026-02-19 04:46:33 +0200

    human-readable lexer errors

--- src/dsl/lexer.zig
+++ src/dsl/lexer.zig
@@ -9,6 +9,8 @@ current: usize = 0,
 start: usize = undefined,
 source: []const u8 = undefined,
 allocator: std.mem.Allocator = undefined,
+lexing_error: ?LexError = null,
+start_of_line: usize = 0,
 
 var allocator_specific: std.heap.GeneralPurposeAllocator(.{}) = .init;
 var token_list: TokenList = .empty;
@@ -17,6 +19,13 @@ pub const LexerErrors = error{
     UnexpectedEndOfFile,
     UnexpectedSymbol,
     InvalidEscapeCharacter,
+    UnexpectedEndOfLine,
+};
+
+pub const LexError = struct {
+    kind: LexerErrors,
+    line: usize,
+    col: usize,
 };
 
 pub const TokenType = enum {
@@ -91,22 +100,22 @@ pub fn init(source: []const u8) Self {
     };
 }
 
-fn atEnd(self: Self) bool {
-    return self.current >= self.source.len;
+inline fn atEnd(self: Self) bool {
+    return self.current >= self.source.len - 1;
 }
 
-inline fn atEndErr(self: Self) LexerErrors!void {
-    if (self.atEnd()) return error.UnexpectedEndOfFile;
+inline fn atEndErr(self: *Self) !void {
+    if (self.atEnd()) return self.fail(error.UnexpectedEndOfFile);
 }
 
-fn advance(self: *Self) LexerErrors!u8 {
+fn advance(self: *Self) !u8 {
     try self.atEndErr();
     const char: u8 = self.source[self.current];
     self.current += 1;
     return char;
 }
 
-fn peek(self: Self) LexerErrors!u8 {
+fn peek(self: *Self) !u8 {
     try self.atEndErr();
     return self.source[self.current];
 }
@@ -125,8 +134,6 @@ fn match(self: *Self, expected: u8) !bool {
     return res;
 }
 
-const LexFuncErrors = LexerErrors || std.mem.Allocator.Error;
-
 fn isDigit(char: u8) bool {
     return char >= '0' and char <= '9';
 }
@@ -135,19 +142,23 @@ fn matchEqual(self: *Self, token_if_match: TokenType, token_if_not_match: TokenT
     return if (try self.match('=')) token_if_match else token_if_not_match;
 }
 
-fn number(self: *Self) LexerErrors!TokenType {
+fn number(self: *Self) !TokenType {
     var dot_seen = false;
     while (true) {
         const char: u8 = self.peek() catch 0;
         if (char == '.') {
             if (dot_seen) {
-                return error.UnexpectedSymbol;
+                try self.fail(error.UnexpectedSymbol);
+                return .EOF;
             }
             _ = try self.advance();
             dot_seen = true;
             continue;
         }
         if (!isDigit(char)) {
+            if (isAlphaNumericUnderscore(char)) {
+                try self.fail(error.UnexpectedSymbol);
+            }
             return if (dot_seen) .DOUBLE else .INTEGER;
         } else {
             _ = try self.advance();
@@ -155,11 +166,14 @@ fn number(self: *Self) LexerErrors!TokenType {
     }
 }
 
-// TODO: \n and such things should be meow
-fn string(self: *Self, str: *?[]u8) LexFuncErrors!TokenType {
+fn string(self: *Self, str: *?[]u8) !TokenType {
     var str_inner: ArrayList(u8) = .empty;
     while (try self.peek() != '"') {
         var c: u8 = try self.advance();
+        if (c == '\n') {
+            self.current -= 1;
+            try self.fail(error.UnexpectedEndOfLine);
+        }
         if (c == '\\') {
             const char = try self.advance();
             c = switch (char) {
@@ -175,7 +189,10 @@ fn string(self: *Self, str: *?[]u8) LexFuncErrors!TokenType {
                 '\'' => '\'',
                 '\"' => '\"',
                 '?' => 0x3F,
-                else => return error.InvalidEscapeCharacter,
+                else => blk: {
+                    try self.fail(error.InvalidEscapeCharacter);
+                    break :blk 0x00;
+                },
             };
         }
         try str_inner.append(self.allocator, c);
@@ -189,16 +206,48 @@ fn isAlphaNumericUnderscore(c: u8) bool {
     return isDigit(c) or (c >= 'a' and c <= 'z') or (c >= 'A' and c <= 'Z') or c == '_';
 }
 
-fn identifier(self: *Self) LexFuncErrors!TokenType {
+fn fail(self: *Self, kind: LexerErrors) !void {
+    self.lexing_error = .{ .col = self.current - self.start_of_line + 1, .kind = kind, .line = self.line };
+    return kind;
+}
+
+fn identifier(self: *Self) !TokenType {
     while (true) {
-        const c = try self.peek();
+        const c = self.peek() catch 0;
         if (!isAlphaNumericUnderscore(c)) break;
         _ = try self.advance();
     }
     return .IDENTIFIER;
 }
 
-pub fn lex(self: *Self) LexFuncErrors!TokenList {
+fn goToEndOfLine(self: *Self) void {
+    while (self.peek() catch '\n' != '\n') {
+        _ = self.advance() catch 0;
+    }
+}
+
+pub fn formatError(self: *Self, writer: *std.Io.Writer) !void {
+    if (self.lexing_error == null) return error.NoErrorProduced;
+    const err = self.lexing_error.?;
+
+    const spaces = try self.allocator.alloc(u8, 7 + err.col - 1);
+    @memset(spaces, ' ');
+
+    self.goToEndOfLine();
+
+    const error_message = try std.fmt.allocPrint(self.allocator,
+        \\{any} at {d}:{d}
+        \\{d: >5}| {s}
+        \\{s}^
+        \\
+        \\
+        \\
+    , .{ err.kind, err.line, err.col, err.line, self.source[self.start_of_line..self.current], spaces });
+    try writer.writeAll(error_message[0..]);
+    try writer.flush();
+}
+
+pub fn lex(self: *Self) !TokenList {
     while (!self.atEnd()) {
         // Only string has such thing cause "\\\\" != \\\\; "\\\\" == \\
         var lexem: ?[]u8 = null;
@@ -210,6 +259,7 @@ pub fn lex(self: *Self) LexFuncErrors!TokenList {
             },
             '\n' => {
                 self.line += 1;
+                self.start_of_line = self.current;
                 continue;
             },
             '!' => try self.matchEqual(.BANG_EQ, .BANG),
@@ -244,8 +294,10 @@ pub fn lex(self: *Self) LexFuncErrors!TokenList {
             '0'...'9' => try self.number(),
             '"' => try self.string(&lexem),
             'a'...'z', 'A'...'Z', '_' => try self.identifier(),
-            else => {
-                break;
+            else => blk: {
+                self.current -= 1;
+                try self.fail(error.UnexpectedSymbol);
+                break :blk .EOF;
             },
         };
         try self.addToken(newToken, self.line, self.start, self.current, lexem);
--- src/dsl/main.zig
+++ src/dsl/main.zig
@@ -3,20 +3,23 @@ const Lexer = @import("lexer.zig");
 
 pub fn main() !void {
     const source =
-        \\+++
-        \\--+
-        \\!!=
-        \\<<<<<<<<
-        \\>>>>=>>>>=>>>^^==|||&&===<<====
-        \\<><><>><><>==<><<<>><<><>===
-        \\ \\\\\\\
-        \\ 67 67 67 67 67 67.676767 67.6767
-        \\ (67.45).meow()
-        \\ "mew" "meow" "" "\n\\\""
+        \\ pub fn meow(arg1: i32) void {
+        \\     if (arg1 != 3.14.15 and 3 | 2 == 7) {
+        \\         print("nya");
+        \\     }
+        \\     print("{}", arg1);
+        \\ }
     ;
     var lexer: Lexer = .init(source[0..]);
-    const res = try lexer.lex();
+    const res = lexer.lex() catch {
+        var buf: [1024]u8 = undefined;
+        var writer_specific = std.fs.File.stdout().writer(&buf);
+        const writer: *std.Io.Writer = &writer_specific.interface;
+
+        try lexer.formatError(writer);
+        return error.LexerFailed;
+    };
     for (res.items) |token| {
-        std.debug.print("{any} on line {d} in pos {d}-{d} {s} {s}\n", .{ token.token_type, token.line, token.position_inside_file[0], token.position_inside_file[1], source[token.position_inside_file[0] - 1 .. token.position_inside_file[1]], token.lexem orelse "null" });
+        std.debug.print("{s} on line {d} in pos {d}-{d} {s} {s}\n", .{ @tagName(token.token_type), token.line, token.position_inside_file[0], token.position_inside_file[1], source[token.position_inside_file[0] - 1 .. token.position_inside_file[1]], token.lexem orelse "null" });
     }
 }
